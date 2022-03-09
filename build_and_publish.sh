5216699c885288b04d231fc0fd775ba5b91d822c
Lukasz Lopatowski <llopat@man.poznan.pl>
2022-03-09 15:44:08 +0100

    Update docker image tag to 1.5.0

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAG=1.4.4
+TAG=1.5.0
 WHAT=janitor
 sudo docker build --rm -t artifactory.software.geant.org/nmaas-docker-local/nmaas-$WHAT:$TAG .
 sudo docker push artifactory.software.geant.org/nmaas-docker-local/nmaas-$WHAT:$TAG
