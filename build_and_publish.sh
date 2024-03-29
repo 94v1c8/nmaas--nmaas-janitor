f4572e7611746320823f163ac1aab46fd7ca4df4
Łukasz Łopatowski <lukasz.lopatowski@gmail.com>
2024-03-29 10:39:35 +0100

    Removed not used script

--- build_and_publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-TAG=1.5.3
-WHAT=janitor
-sudo docker build --rm -t artifactory.software.geant.org/nmaas-docker-local/nmaas-$WHAT:$TAG .
-sudo docker push artifactory.software.geant.org/nmaas-docker-local/nmaas-$WHAT:$TAG
